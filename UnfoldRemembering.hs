b8634aed49fed1a5ec5e49f5a196d048720d31f4
Amar Shah <amar47shah@gmail.com>
2017-07-02 01:09:24 +0200

    Demonstrate duality of foldr and unfoldr

--- UnfoldRemembering.hs
+++ UnfoldRemembering.hs
@@ -1,9 +1,10 @@
 module UnfoldRemembering (unfoldR, unfoldRLast) where
 
 import Control.Arrow (second)
+import Control.Category ((<<<), (>>>))
 import Data.List (unfoldr)
 import Data.Tuple (swap)
-import Safe (lastMay)
+import Safe (headMay, lastMay, tailMay)
 
 -- | Unfold and return a list of result-intermediate tuples.
 -- Compare to unfoldr :: (b -> Maybe (a, b)) -> b -> [a]
@@ -32,3 +33,18 @@ unfoldRLast' f x =
   case f x of
     (Nothing, x') -> ([], x')
     (Just y , x') -> let (ys, x'') = unfoldRLast' f x' in (y:ys, x'')
+
+-- | Fold and Unfold as explicit duals.
+foldr' :: (Maybe (a, b) -> b) -> [a] -> b
+foldr' f = f <<< fmap (\(x, xs) -> (x, foldr' f xs)) <<< headAndTailMay
+
+unfoldr' :: (b -> Maybe (a, b)) -> b -> [a]
+unfoldr' f = f >>> fmap (\(x, xs) -> (x, unfoldr' f xs)) >>> consMay
+
+headAndTailMay :: [a] -> Maybe (a, [a])
+headAndTailMay []     = Nothing
+headAndTailMay (x:xs) = Just (x, xs)
+
+consMay :: Maybe (a, [a]) -> [a]
+consMay Nothing = []
+consMay (Just (x, xs)) = x:xs
